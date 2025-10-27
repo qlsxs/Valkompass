722aaf4458e6e20faa029eec37516de3c2345eb0
Leo Trolin <leo.trolin@gmail.com>
2025-10-27 19:24:29 +0100

    Serve under subpath

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 from compass_server import app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == '__main__':
-    app.run(host=os.getenv("HOST"), port=os.getenv("PORT"), debug=os.getenv("DEBUG"))
+    app2 = DispatcherMiddleware(None, {os.getenv("SUBPATH"): app})
+    run_simple(os.getenv("HOST"), int(os.getenv("PORT")), app2, use_reloader=os.getenv("DEBUG")=="true", use_debugger=os.getenv("DEBUG")=="true")
