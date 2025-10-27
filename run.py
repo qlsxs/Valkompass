f221f9d1036832588cb601fe4b2faea304390e1a
Leo Trolin <leo.trolin@gmail.com>
2025-10-27 19:31:34 +0100

    Use waitress to serve

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
 from compass_server import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+from waitress import serve
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == '__main__':
     app2 = DispatcherMiddleware(None, {os.getenv("SUBPATH"): app})
-    run_simple(os.getenv("HOST"), int(os.getenv("PORT")), app2, use_reloader=os.getenv("DEBUG")=="true", use_debugger=os.getenv("DEBUG")=="true")
+    if os.getenv("DEBUG") == "true":
+        from werkzeug.serving import run_simple
+        run_simple(os.getenv("HOST"), int(os.getenv("PORT")), app2, use_reloader=True, use_debugger=True)
+    else:
+        serve(app2, host=os.getenv("HOST"), port=os.getenv("PORT"))
